4a49b57fde69006e25f15734739397a01ea5a505
Melinda Chang <melindachang.hy@gmail.com>
2023-01-09 22:46:18 -0800

    skeleton (nonfunctional)

--- src/plugin.js
+++ src/plugin.js
@@ -1,20 +1,20 @@
-import { parse, Compiler } from 'albio/compiler';
+import { parse_module, record_entry } from './compile';
 
 export const albio = () => {
   return {
     name: 'vite-plugin-albio',
     enforce: 'pre',
-    transformIndexHtml: {
-      order: 'post',
-      handler: (html, ctx) => {
-        const head = html.match(/<head[^>]*>[\s\S]*<\/head>/gi);
-        let scripts = [
-          `<script src="./${ctx.fileName.replace('.html', '')}.js"></script>`,
-          '<script>registerComponent()\nmountComponent()</script>',
-        ];
-
-        return `<!DOCTYPE html><html>${head}<body>${scripts.join('\n')}</body></html>`;
-      },
+    transformIndexHtml: (html, ctx) => {
+      record_entry(html, ctx);
+      const head = html.match(/<head[^>]*>[\s\S]*<\/head>/gi);
+      const scripts = [
+        `<script src="./${ctx.fileName.replace('.html', '')}.js" type="module"></script>`,
+        '<script>registerComponent()\nmountComponent()</script>',
+      ];
+      return `<!DOCTYPE html><html>${head}<body>${scripts.join('')}</body></html>`;
+    },
+    transform: (code, id) => {
+      parse_module(code, id);
     },
   };
 };
